2b04c772eb519b5402b197c1e0452c8060d5134f
Oliver Pitsch <oliver.pitsch@trustedshops.de>
2018-01-28 12:50:23 +0100

    update rakefile and config

--- config.rb
+++ config.rb
@@ -13,7 +13,9 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-require 'middleman-gh-pages'
+
+activate :relative_assets
+set :relative_links, true
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
--- rakefile.rb
+++ rakefile.rb
@@ -1,1 +1,6 @@
 require 'middleman-gh-pages'
+
+# Ensure builds are skipped when publishing to the gh-pages branch
+ENV["COMMIT_MESSAGE_SUFFIX"] = "[skip ci]"
+# Ignore errors about dirty builds (not recommended)
+ENV["ALLOW_DIRTY"] = "true"
